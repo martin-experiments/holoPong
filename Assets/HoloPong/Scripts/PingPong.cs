63531640ef785441a873d5fa8663eb5dd7ad0ea5
Alexander Martin <alex@suitupalex.com>
2016-07-24 14:45:32 -0500

    init: Initial project setup.

---
+++ Assets/HoloPong/Scripts/PingPong.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPong : MonoBehaviour {
+  private Rigidbody body;
+
+  void Start () {
+    this.body = GetComponent<Rigidbody>();
+    Invoke("StartMoving", 5);
+  }
+
+  void Update () {
+  }
+
+  private void StartMoving() {
+    this.body.velocity = new Vector3(1.0f, 0, 0);
+  }
+}
